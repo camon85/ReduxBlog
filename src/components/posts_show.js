f9f10e15764cb64e09120bf393efa0df8f8bf5ae
Jooyong Sung <camon85@gmail.com>
2017-12-14 17:43:54 +0900

    delete 구현

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import {connect} from 'react-redux';
+import {fetchPost, deletePost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)
+      .then(() => this.context.router.push('/'));
+  }
+
   render() {
-    return <div>Show post {this.props.params.id}</div>
+    const { post } = this.props;
+
+    if (!this.props.post) {
+      return <div>Loading... </div>
+    }
+
+    const {title, categories, content } = post;
+
+    return (
+      <div>
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>Delete Post</button>
+        <h3>{title}</h3>
+        <h6>Categories: {categories}</h6>
+        <p>{content}</p>
+        </div>
+    );
   }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+  return {
+    post: state.posts.post
+  };
+}
+
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
