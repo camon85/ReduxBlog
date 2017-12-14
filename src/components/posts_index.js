767d910770504c8783d9c9ac0d2b2af3cd44e496
Jooyong Sung <camon85@gmail.com>
2017-12-14 16:56:25 +0900

    dynamic route

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,29 +1,51 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchPosts } from "../actions";
-import { Link } from 'react-router';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from "../actions";
+import {Link} from 'react-router';
 
-class PostsIndex extends  Component {
-    componentWillMount() {
-        this.props.fetchPosts();
-    }
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="text-xs-right">
-                    <Link to="/posts/new" className="btn btn-primary">
-                        Add a Post
-                    </Link>
-                </div>
-                List of log posts.
-            </div>
-        );
-    }
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={'posts/' + post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+          </Link>
+        </li>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
 
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return {
+    posts: state.posts.all
+  };
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
 
 /////////////////////////////////////////////////////
 // 보일러플레이트를 아래처럼 한줄로 바꿀수도 있다.
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 import PostIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 export default (
-    <Route path="/" component={App}>
-        <IndexRoute component={PostIndex}/>
-        <Route path="posts/new" component={PostsNew}/>
-    </Route>
+  <Route path="/" component={App}>
+    <IndexRoute component={PostIndex}/>
+    <Route path="posts/new" component={PostsNew}/>
+    <Route path="posts/:id" component={PostsShow}/>
+  </Route>
 )
 
+// <Route path="posts/:id" component={PostsShow}/>      =>  this.props.props.params.id
+// <Route path="posts/:post_id" component={PostsShow}/> =>  this.props.props.params.post_id
